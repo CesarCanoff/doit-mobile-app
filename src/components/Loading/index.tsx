896adf24dd0eac428a0ed053717fdd56fbcc8605
CesarCanoff <canoff.cesar@gmail.com>
2024-01-25 02:13:59 -0400

    Fix: Loading Components Colors

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,10 +1,12 @@
 import { ActivityIndicator, View } from "react-native";
 import { STYLES } from "./styles";
 
+import { THEME } from "@themes/index";
+
 export const Loading = () => {
   return (
     <View style={STYLES.LOADING_CONTAINER}>
-      <ActivityIndicator size="large" color="blue" />
+      <ActivityIndicator size="large" color={THEME.COLORS.PRIMARY} />
     </View>
   );
 };
--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from "react-native";
 
+import { THEME } from "@themes/index";
+
 export const STYLES = StyleSheet.create({
   LOADING_CONTAINER: {
     height: "100%",
-    backgroundColor: "#000",
+    backgroundColor: THEME.COLORS.BACKGROUND,
     justifyContent: "center",
     alignItems: "center",
   },
